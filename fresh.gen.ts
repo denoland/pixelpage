0693f67d99dd5d58d87626f8df2600e9a2195d60
Luca Casonato <hello@lcas.dev>
2023-06-15 21:32:28 +0200

    Update fresh to 1.2

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/api/listen.ts";
 import * as $1 from "./routes/api/update.ts";
 import * as $2 from "./routes/index.tsx";
@@ -18,7 +17,6 @@ const manifest = {
     "./islands/Pixels.tsx": $$0,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
